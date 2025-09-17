66132bb4006374e60b33e957b77a9b84f37dcd05
juuliawsss <juuliawylipelkonen@gmail.com>
2025-09-17 10:23:32 +0300

    a

--- php/kirjaudu.php
+++ php/kirjaudu.php
@@ -2,21 +2,21 @@
 // Tarkistetaan onko lomake lähetetty
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Otetaan vastaan lomakkeen tiedot
-    $kayttajatunnus = $_POST["kayttajatunnus"];
-    $salasana = $_POST["salasana"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
     // Tarkistetaan käyttäjätunnus ja salasana
-    if ($kayttajatunnus == "admin" && $salasana == "1234") {
+    if ($username === "admin" && $password === "kissa123") {
         // Kirjautuminen onnistui
-        echo "Kirjautuminen onnistui! Ohjataan palkkalaskuriin...";
+        echo "<h1>Tervetuloa admin!</h1>";
+        echo "<p>Sinut ohjataan palkkalaskuriin muutaman sekunnin kuluttua...</p>";
 
-        // Ohjaa käyttäjän palkkalaskuriin 2 sekunnin kuluttua
-        echo '<meta http-equiv="refresh" content="2;url=palkkalaskuri_kirjautuminen.html">';
+        // Ohjaus 2 sekunnin kuluttua
+        echo '<meta http-equiv="refresh" content="2;url=palkkalaskuri.php">';
         exit();
     } else {
         // Kirjautuminen epäonnistui
-        echo "<p style='color:red;'>Virheellinen käyttäjätunnus tai salasana!</p>";
-        echo '<p><a href="palkkalaskuri_kirjautuminen.html">Yritä uudelleen</a></p>';
+        echo "<p>Virheellinen käyttäjätunnus tai salasana.</p>";
     }
 }
 ?>
