d10c1a83dd5640fa08fa4f348b4748c22fd453ad
juuliawsss <juuliawylipelkonen@gmail.com>
2025-11-25 13:20:03 +0200

    w

--- wordpress/wordpress/wp-config.php
+++ wordpress/wordpress/wp-config.php
@@ -88,3 +88,15 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
